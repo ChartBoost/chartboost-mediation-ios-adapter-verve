a78e98cc74ba4dc257954608b559318367411153
Alex Rice <rice.alexanderb@gmail.com>
2023-05-17 17:53:32 -0700

    Internalize VerveLogLevel

--- Source/VerveAdapterConfiguration.swift
+++ Source/VerveAdapterConfiguration.swift
@@ -6,32 +6,32 @@
 import Foundation
 import HyBid
 
-// A mapping of HyBid log levels to an externally-visible type for publishers to use when configuring this adapter
-@objc public enum VerveLogLevel: Int {
-    case none = 0
-    case error
-    case warning
-    case info
-    case debug
-
-    var hyBidLogLevel: HyBidLogLevel {
-        switch self {
-        case .none:
-            return HyBidLogLevelNone
-        case .error:
-            return HyBidLogLevelError
-        case .warning:
-            return HyBidLogLevelWarning
-        case .info:
-            return HyBidLogLevelInfo
-        case .debug:
-            return HyBidLogLevelDebug
-        }
-    }
-}
-
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
 @objc public class VerveAdapterConfiguration: NSObject {
+
+    // A mapping of HyBid log levels to an externally-visible type for publishers to use when configuring this adapter
+    @objc public enum VerveLogLevel: Int {
+        case none = 0
+        case error
+        case warning
+        case info
+        case debug
+
+        var hyBidLogLevel: HyBidLogLevel {
+            switch self {
+            case .none:
+                return HyBidLogLevelNone
+            case .error:
+                return HyBidLogLevelError
+            case .warning:
+                return HyBidLogLevelWarning
+            case .info:
+                return HyBidLogLevelInfo
+            case .debug:
+                return HyBidLogLevelDebug
+            }
+        }
+    }
     
     /// Flag that can optionally be set to enable the partner's test mode.
     /// Disabled by default.
