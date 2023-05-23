ce541b46330624ab3a0415a596117857212c3f35
Alex Rice <rice.alexanderb@gmail.com>
2023-05-23 11:36:50 -0700

    version to 4.2.18.0.0

--- Source/VerveAdapter.swift
+++ Source/VerveAdapter.swift
@@ -13,12 +13,12 @@ final class VerveAdapter: PartnerAdapter {
     private let APP_TOKEN_KEY: String = "app_token"
 
     /// The version of the partner SDK.
-    let partnerSDKVersion = "2.17.0"
+    let partnerSDKVersion = "2.18.0"
     
     /// The version of the adapter.
     /// It should have either 5 or 6 digits separated by periods, where the first digit is Chartboost Mediation SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
     /// Format: `<Chartboost Mediation major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>` where `.<Partner build version>` is optional.
-    let adapterVersion = "4.2.17.0.0"
+    let adapterVersion = "4.2.18.0.0"
     
     /// The partner's unique identifier.
     let partnerIdentifier = "verve"
