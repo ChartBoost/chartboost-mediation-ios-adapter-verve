cbfbd7755c23eb6c7e219f3ccaef61900c7e9f5f
Daniel Barros <daniel.barros@chartboost.com>
2024-05-03 11:15:02 -0700

    Adopt new Configuration protocol

--- Source/VerveAdapterConfiguration.swift
+++ Source/VerveAdapterConfiguration.swift
@@ -3,11 +3,12 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
+import ChartboostMediationSDK
 import Foundation
 import HyBid
 
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
-@objc public class VerveAdapterConfiguration: NSObject {
+@objc public class VerveAdapterConfiguration: NSObject, PartnerAdapterConfiguration {
 
     /// The version of the partner SDK.
     @objc public static var partnerSDKVersion: String {
